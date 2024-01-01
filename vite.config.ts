1a9fae30ccf5dff5492685ab31487ed6aea9576f
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2024-01-01 12:38:14 -0500

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), splitVendorChunkPlugin()],
+  build: { manifest: true },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
